1d21e80f4393c45488bdf3fee0a00fdfe23a8418
rage73 <raghavagrawal2000ra@gmail.com>
2021-08-29 04:18:49 +0530

    styling

--- public/script.js
+++ public/script.js
@@ -12,25 +12,96 @@ let SEA = ['Bren Esports', 'Paper Rex'];
 let LATAM = ['KRÜ Esports'];
 let JP = ['ZETA DIVISION', 'Crazy Raccoon'];
 
-var pool1,pool2,pool3,pool4;
-var g1,g2,g3,g4;
+var pool1, pool2, pool3, pool4;
+var g1, g2, g3, g4;
 
 function makePools() {
-    pool1 = [{r:'NA',p:0,s:false}, {r:'EMEA',p:0,s:false}, {r:'KR',p:0,s:false}, {r:'BR',p:0,s:false}];
-    pool2 = [{r:'NA',p:1,s:false}, {r:'EMEA',p:1,s:false}, {r:'LATAM',p:0,s:false}, {r:'SEA',p:0,s:false}];
-    pool3 = [{r:'JP',p:0,s:false}, {r:'BR',p:1,s:false}, {r:'KR',p:1,s:false}, {r:'EMEA',p:2,s:false}];
-    pool4 = [{r:'NA',p:2,s:false}, {r:'SEA',p:1,s:false}, {r:'JP',p:1,s:false}, {r:'EMEA',p:3,s:false}];
+    pool1 = [{
+        r: 'NA',
+        p: 0,
+        s: false
+    }, {
+        r: 'EMEA',
+        p: 0,
+        s: false
+    }, {
+        r: 'KR',
+        p: 0,
+        s: false
+    }, {
+        r: 'BR',
+        p: 0,
+        s: false
+    }];
+    pool2 = [{
+        r: 'NA',
+        p: 1,
+        s: false
+    }, {
+        r: 'EMEA',
+        p: 1,
+        s: false
+    }, {
+        r: 'LATAM',
+        p: 0,
+        s: false
+    }, {
+        r: 'SEA',
+        p: 0,
+        s: false
+    }];
+    pool3 = [{
+        r: 'JP',
+        p: 0,
+        s: false
+    }, {
+        r: 'BR',
+        p: 1,
+        s: false
+    }, {
+        r: 'KR',
+        p: 1,
+        s: false
+    }, {
+        r: 'EMEA',
+        p: 2,
+        s: false
+    }];
+    pool4 = [{
+        r: 'NA',
+        p: 2,
+        s: false
+    }, {
+        r: 'SEA',
+        p: 1,
+        s: false
+    }, {
+        r: 'JP',
+        p: 1,
+        s: false
+    }, {
+        r: 'EMEA',
+        p: 3,
+        s: false
+    }];
 }
 
 function getReg(reg) {
     switch (reg) {
-        case 'NA': return NA;
-        case 'EMEA': return EMEA;
-        case 'KR': return KR;
-        case 'BR': return BR;
-        case 'SEA': return SEA;
-        case 'LATAM': return LATAM;
-        case 'JP': return JP;
+        case 'NA':
+            return NA;
+        case 'EMEA':
+            return EMEA;
+        case 'KR':
+            return KR;
+        case 'BR':
+            return BR;
+        case 'SEA':
+            return SEA;
+        case 'LATAM':
+            return LATAM;
+        case 'JP':
+            return JP;
     }
 }
 
@@ -39,14 +110,13 @@ function getRandVal(size) {
 }
 
 
-function getTeam(pool,regCnt) {
-    
+function getTeam(pool, regCnt) {
+
     var team = {};
-    var itr=0;
+    var itr = 0;
     do {
         itr++;
-        if(team)
-        {
+        if (team) {
             regCnt[team.r]--;
         }
         var idx = getRandVal(pool.length);
@@ -54,28 +124,25 @@ function getTeam(pool,regCnt) {
         team.name = getReg(team.r)[pool[idx].p];
         regCnt[team.r]++;
     }
-    while(isItNotOk(regCnt) && itr<=pool.length);
+    while (isItNotOk(regCnt) && itr <= pool.length);
 
-    if(itr>pool.length)
-    {
+    if (itr > pool.length) {
         return;
     }
 
-    pool.splice(idx,1);
+    pool.splice(idx, 1);
 
     return team;
 }
 
 function isItNotOk(regCnt) {
-    for(r in regCnt) {
-        if(r=='EMEA') {
-            if(regCnt[r]>2) {
+    for (r in regCnt) {
+        if (r == 'EMEA') {
+            if (regCnt[r] > 2) {
                 return true;
             }
-        }
-        else {
-            if(regCnt[r]>1)
-            {
+        } else {
+            if (regCnt[r] > 1) {
                 return true;
             }
         }
@@ -84,50 +151,57 @@ function isItNotOk(regCnt) {
 }
 
 function randGrps() {
-    let regCnt = {'NA':0, 'EMEA':0, 'KR':0, 'BR':0, 'SEA':0, 'LATAM':0, 'JP':0 };
+    let regCnt = {
+        'NA': 0,
+        'EMEA': 0,
+        'KR': 0,
+        'BR': 0,
+        'SEA': 0,
+        'LATAM': 0,
+        'JP': 0
+    };
     var group = [];
 
-    let t1,t2,t3,t4;
-    t1=getTeam(pool1,regCnt);
-    t2=getTeam(pool2,regCnt);
-    t3=getTeam(pool3,regCnt);
-    t4=getTeam(pool4,regCnt);
+    let t1, t2, t3, t4;
+    t1 = getTeam(pool1, regCnt);
+    t2 = getTeam(pool2, regCnt);
+    t3 = getTeam(pool3, regCnt);
+    t4 = getTeam(pool4, regCnt);
 
-    if(t1 == null || t2 == null || t3 == null || t4 == null)
-    {
+    if (t1 == null || t2 == null || t3 == null || t4 == null) {
         return;
     }
-    
-    group.push(t1.name,t2.name,t3.name,t4.name);
+
+    group.push(t1.name, t2.name, t3.name, t4.name);
 
     return group;
 }
 
 function getGrps() {
     makePools();
-    g1=randGrps();
-    g2=randGrps();
-    g3=randGrps();
-    g4=randGrps();
+    g1 = randGrps();
+    g2 = randGrps();
+    g3 = randGrps();
+    g4 = randGrps();
 
-    if(g1 == null || g2 == null || g3 == null || g4 == null)
-    {
+    if (g1 == null || g2 == null || g3 == null || g4 == null) {
         getGrps();
     }
 }
 
-function addToList(group, list)
-{
+function addToList(group, list) {
     list.empty();
     group.forEach((element) => {
-        list.append($('<li>', {text: element}));
+        list.append($('<li>', {
+            text: element
+        }));
     });
 }
 
 draw.click(() => {
     getGrps();
-    addToList(g1,listA);
-    addToList(g2,listB);
-    addToList(g3,listC);
-    addToList(g4,listD);
+    addToList(g1, listA);
+    addToList(g2, listB);
+    addToList(g3, listC);
+    addToList(g4, listD);
 })
